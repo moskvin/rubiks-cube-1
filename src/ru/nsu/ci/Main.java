d6f649c5ac252b23b4bd1e0f5ced9ed03338e3b8
Evdokimov <evsese@gmail.com>
2011-04-01 12:11:45 +0700

    =Peredelali chtobi rabotalo

--- src/ru/nsu/ci/Main.java
+++ src/ru/nsu/ci/Main.java
@@ -1,4 +1,4 @@
-package ru.nsu.ci;
+/*package ru.nsu.ci;
 
 import ru.nsu.ci.graphics.MainFrame;
 
@@ -6,8 +6,29 @@ import ru.nsu.ci.graphics.MainFrame;
  * Здесь должна запускаться графика,
  * возможно инициализация компонентов
  */
+/*
 public class Main {
 	public static void main(String[] args) {
 		new MainFrame();
+	}
+}
+*/
+package ru.nsu.ci;
+
+import ru.nsu.ci.graphics.MainFrame;
+import ru.nsu.ci.translator.SI;
+import ru.nsu.ci.translator.ParseException;
+import ru.nsu.ci.translator.TokenMgrError;
+
+/**
+ * Здесь должна запускаться графика,
+ * возможно инициализация компонентов
+ */
+public class Main {
+	public static void main(String[] args) throws ParseException, TokenMgrError, NumberFormatException
+{
+		new MainFrame();
+        SI si=new SI(System.in);
+        si.start(System.out);
 	}
 }
