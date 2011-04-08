fc9328b49410acd3ad3578dea2112d6c74351197
Evdokimov <evsese@gmail.com>
2011-04-08 10:52:24 +0700

    Dobavleni biblioteki JOGL

--- src/ru/nsu/ci/Main.java
+++ src/ru/nsu/ci/Main.java
@@ -1,18 +1,3 @@
-/*package ru.nsu.ci;
-
-import ru.nsu.ci.graphics.MainFrame;
-
-/**
- * Здесь должна запускаться графика,
- * возможно инициализация компонентов
- */
-/*
-public class Main {
-	public static void main(String[] args) {
-		new MainFrame();
-	}
-}
-*/
 package ru.nsu.ci;
 
 import ru.nsu.ci.graphics.MainFrame;
