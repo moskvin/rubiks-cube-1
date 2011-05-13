0a22ebafcb764cd58effff03324c4a4935d1a405
Evdokimov <evsese@gmail.com>
2011-05-13 10:24:55 +0700

    integrirovanie

--- src/ru/nsu/ci/RubicNode.java
+++ src/ru/nsu/ci/RubicNode.java
@@ -2,8 +2,16 @@ package ru.nsu.ci;
 
 public class RubicNode{
 	int num=0;
-
+	int num1=0;
 	
+	public int getNum1() {
+		return num1;
+	}
+
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+
 	public int getNum() {
 		return num;
 	}
